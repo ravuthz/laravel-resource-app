abdc9d7bb582b5d6476739fb779531267008cdc6
Yo Vannaravuth <ravuthz@gmail.com>
2018-02-08 10:22:21 +0000

    Implement redirect and callback with to access implicit grant token

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,3 +16,34 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+$authorizeServer = 'http://all-laravel-ravuthz.c9users.io:8081';
+$currentSystem = 'http://all-laravel-ravuthz.c9users.io:8082';
+
+Route::get('/redirect', function () use ($authorizeServer, $currentSystem) {
+    $query = http_build_query([
+        'client_id' => '4',
+        'redirect_uri' => $currentSystem . '/callback',
+        'response_type' => 'code',
+        'scope' => '',
+    ]);
+
+    return redirect($authorizeServer . '/oauth/authorize?' . $query);
+})->name('get.token');
+
+Route::get('/callback', function (Request $request) use ($authorizeServer, $currentSystem) {
+    $http = new GuzzleHttp\Client;
+
+    $response = $http->post($authorizeServer . '/oauth/token', [
+        'form_params' => [
+            'grant_type' => 'authorization_code',
+            'client_id' => '4',
+            'client_secret' => 's9cajLPMRy498RBdBoHf3nkrFDLqOczyOZsFpi7l',
+            'redirect_uri' =>  $currentSystem . '/callback',
+            'code' => $request->code,
+        ],
+    ]);
+
+    return json_decode((string) $response->getBody(), true);
+});
+
